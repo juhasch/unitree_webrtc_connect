0535fdbeceedafce7994c4a3109c174d5b2d09c2
Juergen Hasch <juergen.hasch@mmwaves.de>
2025-12-23 20:53:50 +0100

    Speedup native Lidar decoder using Numba

--- unitree_webrtc_connect/lidar/lidar_decoder_native.py
+++ unitree_webrtc_connect/lidar/lidar_decoder_native.py
@@ -22,6 +22,12 @@
 import numpy as np
 import lz4.block
 
+try:
+    from numba import jit
+    NUMBA_AVAILABLE = True
+except ImportError:
+    NUMBA_AVAILABLE = False
+
 def decompress(compressed_data, decomp_size):
     decompressed = lz4.block.decompress(
         compressed_data,
@@ -29,13 +35,20 @@ def decompress(compressed_data, decomp_size):
     )
     return decompressed
 
-def bits_to_points(buf, origin, resolution=0.05):
-    buf = np.frombuffer(bytearray(buf), dtype=np.uint8)
-    nonzero_indices = np.nonzero(buf)[0]
-    points = []
+@jit(nopython=True, cache=True)
+def _bits_to_points_numba(buf_array):
+    max_points = len(buf_array) * 8
+    points_x = np.empty(max_points, dtype=np.int32)
+    points_y = np.empty(max_points, dtype=np.int32)
+    points_z = np.empty(max_points, dtype=np.int32)
 
-    for n in nonzero_indices:
-        byte_value = buf[n]
+    point_count = 0
+
+    for n in range(len(buf_array)):
+        byte_value = buf_array[n]
+        if byte_value == 0:
+            continue
+
         z = n // 0x800
         n_slice = n % 0x800
         y = n_slice // 0x10
@@ -44,9 +57,40 @@ def bits_to_points(buf, origin, resolution=0.05):
         for bit_pos in range(8):
             if byte_value & (1 << (7 - bit_pos)):
                 x = x_base + bit_pos
-                points.append((x,y,z))
+                points_x[point_count] = x
+                points_y[point_count] = y
+                points_z[point_count] = z
+                point_count += 1
 
-    return np.array(points) * resolution + origin
+    if point_count > 0:
+        return np.column_stack((points_x[:point_count], points_y[:point_count], points_z[:point_count]))
+    else:
+        return np.empty((0, 3), dtype=np.int32)
+
+def bits_to_points(buf, origin, resolution=0.05):
+    buf = np.frombuffer(bytearray(buf), dtype=np.uint8)
+
+    if NUMBA_AVAILABLE:
+        points = _bits_to_points_numba(buf)
+    else:
+        nonzero_indices = np.nonzero(buf)[0]
+        points_list = []
+
+        for n in nonzero_indices:
+            byte_value = buf[n]
+            z = n // 0x800
+            n_slice = n % 0x800
+            y = n_slice // 0x10
+            x_base = (n_slice % 0x10) * 8
+
+            for bit_pos in range(8):
+                if byte_value & (1 << (7 - bit_pos)):
+                    x = x_base + bit_pos
+                    points_list.append((x, y, z))
+
+        points = np.array(points_list) if points_list else np.empty((0, 3), dtype=np.int32)
+
+    return points * resolution + origin
 
 class LidarDecoder:
     def decode(self, compressed_data, data):
